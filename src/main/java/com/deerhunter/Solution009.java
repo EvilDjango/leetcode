909ea72929905c5869dee56397027b5d2a2579b5
xuejunc <deerhunter0837@gmail.com>
2019-08-14 16:34:37 +0800

    新增了两个题目解答

---
+++ src/main/java/com/deerhunter/Solution009.java
@@ -0,0 +1,78 @@
+package com.deerhunter;
+
+import java.util.regex.Matcher;
+
+/**
+ * Oops, forgot to write comment. Good luck, bro.
+ * <p>
+ * Copyright (c) 2019, deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @date 2019-08-14
+ */
+public class Solution009 {
+    public boolean isPalindrome(int x) {
+        String s = x + "";
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+        if (x < 0) {
+            return false;
+        }
+        // 高位
+        int high = getBits(x) - 1;
+        // 低位
+        int low = 0;
+        while (high >= low) {
+            if (getNumByBit(x, high) != getNumByBit(x, low)) {
+                return false;
+            }
+            high--;
+            low++;
+        }
+        return true;
+    }
+
+
+    /**
+     * 获取x右起第i位的数字（i从0开始计）
+     *
+     * @param x
+     * @param i
+     * @return
+     */
+    public static int getNumByBit(int x, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        // 去除高位
+        x %= Math.pow(10, i + 1);
+        if (i > 0) {
+            // 去除低位
+            x /= Math.pow(10, i);
+        }
+        return x;
+    }
+
+    public static int getBits(int x) {
+        int bits = 1;
+        while ((x /= 10) != 0) {
+            bits++;
+        }
+        return bits;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(10, 0));
+    }
+}
---
+++ src/test/java/com/deerhunter/Solution009Test.java
@@ -0,0 +1,52 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comment. Good luck, bro.
+ * <p>
+ * Copyright (c) 2019, deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @date 2019-08-14
+ */
+class Solution009Test {
+    private Solution009 solution = new Solution009();
+
+    @Test
+    void isPalindrome() {
+        assertTrue(solution.isPalindrome(121));
+        assertTrue(solution.isPalindrome(0));
+        assertTrue(solution.isPalindrome(1));
+        assertFalse(solution.isPalindrome(-1));
+        assertFalse(solution.isPalindrome(10));
+    }
+
+    @Test
+    void isPalindrome2() {
+        assertTrue(solution.isPalindrome2(121));
+        assertTrue(solution.isPalindrome2(0));
+        assertTrue(solution.isPalindrome2(1));
+        assertFalse(solution.isPalindrome2(-1));
+        assertFalse(solution.isPalindrome2(10));
+    }
+
+    @Test
+    void bits() {
+        assertEquals(1, Solution009.getBits(0));
+        assertEquals(1, Solution009.getBits(3));
+        assertEquals(2, Solution009.getBits(99));
+        assertEquals(3, Solution009.getBits(100));
+    }
+
+    @Test
+    void getNumByBit() {
+        assertEquals(9, Solution009.getNumByBit(9, 0));
+        assertEquals(9, Solution009.getNumByBit(91, 1));
+        assertEquals(0, Solution009.getNumByBit(91, -1));
+        assertEquals(0, Solution009.getNumByBit(91, 5));
+        assertEquals(2, Solution009.getNumByBit(23291, 4));
+    }
+}
