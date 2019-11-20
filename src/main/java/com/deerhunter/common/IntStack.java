d0be98900385f48937ca5b1f4311068b94921932
xuejunc <deerhunter0837@gmail.com>
2019-11-20 13:11:04 +0800

    1. 新增了一个简易的栈IntStack，效率高于jdk的Stack类 2. 新增41和42的解法

---
+++ src/main/java/com/deerhunter/common/IntStack.java
@@ -0,0 +1,51 @@
+package com.deerhunter.common;
+
+/**
+ * int栈
+ * <p>
+ * Copyright (c) 2019, deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @date 2019-11-20
+ */
+public class IntStack {
+    private int[] stack;
+    private int capacity;
+    private int top;
+
+    public IntStack(int size) {
+        this.stack = new int[size];
+        capacity = stack.length;
+        top = -1;
+    }
+
+    public void push(int i) {
+        if (top == capacity - 1) {
+            throw new IllegalStateException("Stack is full");
+        }
+        stack[++top] = i;
+    }
+
+    public int peek() {
+        if (top < 0) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack[top];
+    }
+
+    public int pop() {
+        if (top < 0) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        top--;
+        return stack[top + 1];
+    }
+
+    public boolean isEmpty() {
+        return top < 0;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
