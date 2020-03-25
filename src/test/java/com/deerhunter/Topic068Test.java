50e94cd455a44440e69354f958341f9216e0eed2
xuejunc <deerhunter0837@gmail.com>
2020-03-25 12:32:26 +0800

    增加了68，69的题解

---
+++ src/test/java/com/deerhunter/Topic068Test.java
@@ -0,0 +1,67 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comment. Good luck, bro.
+ * <p>
+ * Copyright (c) 2019, deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @date 2020-03-24
+ */
+class Topic068Test {
+
+    @Test
+    void solution1() {
+        Topic068.Solution1 instance = new Topic068.Solution1();
+        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        List<String> expected = Arrays.asList(
+                "This    is    an",
+                "example  of text",
+                "justification.  "
+        );
+        List<String> actualLines = instance.fullJustify(words, 16);
+        assertLinesMatch(expected, actualLines);
+
+        words = new String[]{"What", "must", "be", "acknowledgment", "shall", "be"};
+        expected = Arrays.asList(
+                "What   must   be",
+                "acknowledgment  ",
+                "shall be        "
+        );
+
+        actualLines = instance.fullJustify(words, 16);
+        assertLinesMatch(expected, actualLines);
+
+        words = new String[]{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a",
+                "computer.", "Art", "is", "everything", "else", "we", "do"};
+        expected = Arrays.asList(
+                "Science  is  what we",
+                "understand      well",
+                "enough to explain to",
+                "a  computer.  Art is",
+                "everything  else  we",
+                "do                  ");
+        actualLines = instance.fullJustify(words, 20);
+        assertLinesMatch(expected, actualLines);
+
+        words = new String[]{"Listen", "to", "many,", "speak", "to", "a", "few."};
+        expected = Arrays.asList(
+                "Listen",
+                "to    ",
+                "many, ",
+                "speak ",
+                "to   a",
+                "few.  "
+        );
+        actualLines = instance.fullJustify(words, 6);
+        assertLinesMatch(expected, actualLines);
+    }
+}
---
+++ src/test/java/com/deerhunter/Topic069Test.java
@@ -0,0 +1,48 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comment. Good luck, bro.
+ * <p>
+ * Copyright (c) 2019, deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @date 2020-03-24
+ */
+class Topic069Test {
+    @Test
+    void solution1() {
+        Topic069.Solution1 instance = new Topic069.Solution1();
+        assertEquals(3,instance.mySqrt(10));
+        assertEquals(0,instance.mySqrt(0));
+        assertEquals(3,instance.mySqrt(9));
+        assertEquals(10,instance.mySqrt(101));
+        assertEquals(1,instance.mySqrt(1));
+        assertEquals(46339,instance.mySqrt(2147395599));
+    }
+
+    @Test
+    void solution2() {
+        Topic069.Solution2 instance = new Topic069.Solution2();
+        assertEquals(3,instance.mySqrt(10));
+        assertEquals(0,instance.mySqrt(0));
+        assertEquals(3,instance.mySqrt(9));
+        assertEquals(10,instance.mySqrt(101));
+        assertEquals(1,instance.mySqrt(1));
+        assertEquals(46339,instance.mySqrt(2147395599));
+    }
+
+    @Test
+    void solution3() {
+        Topic069.Solution3 instance = new Topic069.Solution3();
+        assertEquals(3,instance.mySqrt(10));
+        assertEquals(0,instance.mySqrt(0));
+        assertEquals(3,instance.mySqrt(9));
+        assertEquals(10,instance.mySqrt(101));
+        assertEquals(1,instance.mySqrt(1));
+        assertEquals(46339,instance.mySqrt(2147395599));
+    }
+}
