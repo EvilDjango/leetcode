410c34d1a9fa2b0a8f1fd300aaeb46feff99ed0f
xuejunc <deerhunter0837@gmail.com>
2021-04-02 18:31:13 +0800

    新增了217-218题解

---
+++ src/main/java/com/deerhunter/Topic217.java
@@ -0,0 +1,45 @@
+package com.deerhunter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 217. 存在重复元素
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ * <p>
+ * Copyright (c) 20120 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2021/3/30 17:54
+ */
+public class Topic217 {
+    public static class Solution1 {
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                if (set.contains(num)) {
+                    return true;
+                }
+                set.add(num);
+            }
+            return false;
+        }
+    }
+}
