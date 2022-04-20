305b6c3fd968d3621119d5da4b71087cb98046aa
xuejunc <deerhunter0837@gmail.com>
2022-04-21 00:05:01 +0800

    新增题解

---
+++ src/test/java/com/deerhunter/M1oyTvTest.java
@@ -0,0 +1,28 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comments. Good luck, bro.
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/4/20 下午11:04
+ */
+class M1oyTvTest {
+    private M1oyTv instance = new M1oyTv();
+
+    void test(BiFunction<String, String, String> function) {
+        assertEquals("BANC", function.apply("ADOBECODEBANC", "ABC"));
+    }
+
+    @Test
+    void minWindow() {
+        test(instance::minWindow);
+    }
+}
---
+++ src/test/java/com/deerhunter/Topic300Test.java
@@ -0,0 +1,34 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comments. Good luck, bro.
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/4/20 下午8:30
+ */
+class Topic300Test {
+    private Topic300 instance=new Topic300();
+
+    void test(Function<int[], Integer> function) {
+        int []nums={1,3,6,7,9,4,10,5,6};
+        assertEquals(6,function.apply(nums));
+    }
+
+    @Test
+    void test1() {
+        test(instance::lengthOfLIS);
+    }
+
+    @Test
+    void test2() {
+        test(instance::lengthOfLIS2);
+    }
+}
