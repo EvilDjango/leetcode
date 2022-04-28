849cd0db51411934677c32430b1f705783be1ddf
xuejunc <deerhunter0837@gmail.com>
2022-04-28 12:47:33 +0800

    新增题解

---
+++ src/main/java/com/deerhunter/Offer048.java
@@ -0,0 +1,79 @@
+package com.deerhunter;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * 剑指 Offer 48. 最长不含重复字符的子字符串
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * s.length <= 40000
+ * 注意：本题与主站 3 题相同：https://leetcode-cn.com/problems/longest-substring-
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/4/28 下午12:12
+ */
+public class Offer048 {
+    public int lengthOfLongestSubstring(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, right = 0;
+        int maxLen = 0;
+        while (right < n) {
+            while (set.contains(chars[right])) {
+                set.remove(chars[left++]);
+            }
+            set.add(chars[right++]);
+            maxLen = Math.max(maxLen, right - left);
+        }
+        return maxLen;
+    }
+
+    /**
+     * 参考最快的提交记录。通过利用前一位置的最大无重复序列来加快运算。
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        Map<Character, Integer> lastSeen = new HashMap<>();
+        int maxLen = 0;
+        // 以前一位置结尾的最长无重复字符串
+        int pre = 0;
+        for (int right = 0; right < n; right++) {
+            int cur = Math.min(right - lastSeen.getOrDefault(chars[right], -1), pre + 1);
+            pre = cur;
+            maxLen = Math.max(maxLen, cur);
+            lastSeen.put(chars[right], right);
+        }
+        return maxLen;
+    }
+}
---
+++ src/main/java/com/deerhunter/Offer049.java
@@ -0,0 +1,51 @@
+package com.deerhunter;
+
+import java.util.HashSet;
+
+/**
+ * 剑指 Offer 49. 丑数
+ * 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: n = 10
+ * 输出: 12
+ * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+ * 说明:
+ * <p>
+ * 1 是丑数。
+ * n 不超过1690。
+ * 注意：本题与主站 264 题相同：https://leetcode-cn.com/problems/ugly-number-ii/
+ * <p>
+ * 通过次数119,807提交次数183,521
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/4/28 上午11:48
+ */
+public class Offer049 {
+    public int nthUglyNumber(int n) {
+        int[] nums = new int[n];
+        nums[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            int next2 = nums[i2] * 2;
+            int next3 = nums[i3] * 3;
+            int next5 = nums[i5] * 5;
+            nums[i]=Math.min(next2,Math.min(next3,next5));
+            if(next2==nums[i]){
+                i2++;
+            }
+            if(next3==nums[i]){
+                i3++;
+            }
+            if(next5==nums[i]){
+                i5++;
+            }
+        }
+        return nums[n - 1];
+    }
+}
