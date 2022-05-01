0fcf6b6facb912193718ff61fe892adf20dfbbf5
xuejunc <deerhunter0837@gmail.com>
2022-05-01 16:59:11 +0800

    新增题解

---
+++ src/test/java/com/deerhunter/Offer044Test.java
@@ -0,0 +1,35 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+import sun.security.krb5.internal.PAEncTSEnc;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comments. Good luck, bro.
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/5/1 下午12:15
+ */
+class Offer044Test {
+    private Offer044 instance=new Offer044();
+    void test(Function<Integer, Integer> function) {
+        assertEquals(5,function.apply(5));
+        assertEquals(9,function.apply(9));
+        assertEquals(1,function.apply(1000000000));
+        assertEquals(2,function.apply(2147483647));
+    }
+
+    @Test
+    void findNthDigit() {
+        test(instance::findNthDigit);
+    }
+    @Test
+    void findNthDigit2() {
+        test(instance::findNthDigit2);
+    }
+}
