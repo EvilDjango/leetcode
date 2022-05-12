a881f07150b452105bd6cccdc9a1f6a92ea9af21
xuejunc <deerhunter0837@gmail.com>
2022-05-13 01:17:25 +0800

    新增题解

---
+++ src/main/java/com/deerhunter/Offer025.java
@@ -0,0 +1,72 @@
+package com.deerhunter;
+
+import com.deerhunter.common.ListNode;
+
+/**
+ * 剑指 Offer 25. 合并两个排序的链表
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ * <p>
+ * 示例1：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ * <p>
+ * 0 <= 链表长度 <= 1000
+ * <p>
+ * 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * <p>
+ * 通过次数262,517提交次数361,455
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/5/13 上午12:35
+ */
+public class Offer025 {
+    /**
+     * 迭代
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+    /**
+     * 递归
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists2(l1, l2.next);
+        return l2;
+    }
+}
