58fb57e375c007e26c8e0c27c1e532d0f78fdd57
xuejunc <deerhunter0837@gmail.com>
2022-06-04 10:31:23 +0800

    新增题解

---
+++ src/test/java/com/deerhunter/Offer016Test.java
@@ -0,0 +1,22 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comments. Good luck, bro.
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/5/17 下午2:04
+ */
+class Offer016Test {
+    private Offer016 instance=new Offer016();
+
+    @Test
+    void myPow() {
+        assertEquals(0.01,instance.myPow(0.1,2));
+    }
+}
