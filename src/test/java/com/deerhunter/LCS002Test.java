b55b44cc494bd903f901b0684846c62d6666a239
xuejunc <deerhunter0837@gmail.com>
2024-06-11 22:41:03 +0800

    新增题解

---
+++ src/test/java/com/deerhunter/LCS002Test.java
@@ -0,0 +1,27 @@
+package com.deerhunter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Oops, forgot to write comments. Good luck, bro.
+ * <p>
+ * Copyright (c) @2022 deerhunter0837@gmail.com All Rights Reserved.
+ *
+ * @author xuejunc
+ * @createTime 2022/6/4 下午12:52
+ */
+class LCS002Test {
+    private LCS002 instance=new LCS002();
+    void test(Function<int[],Integer> function){
+        assertEquals(2,function.apply(new int[]{3,13,7,11,1,5,5,3}));
+    }
+
+    @Test
+    void test1() {
+        test(instance::halfQuestions);
+    }
+}
